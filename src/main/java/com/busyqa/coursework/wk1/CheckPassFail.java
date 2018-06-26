d399c5c08920830380f3ce6273c422e23b7a13bd
J <jeames.busyqa@gmail.com>
2018-06-26 15:40:23 -0400

    upload exercise files

---
+++ src/main/java/com/busyqa/coursework/wk1/CheckPassFail.java
@@ -0,0 +1,15 @@
+package com.busyqa.coursework.wk1;
+
+public class CheckPassFail {  // Save as "CheckPassFail.java"
+    public static void main(String[] args) {  // Program entry point
+        int mark = 49;  // Set the value of "mark" here!
+        System.out.println("The mark is " + mark);
+
+//        if ( ...... ) {
+//            System.out.println( ...... );
+//        } else {
+//            System.out.println( ...... );
+//        }
+        System.out.println( "done" );
+    }
+}
