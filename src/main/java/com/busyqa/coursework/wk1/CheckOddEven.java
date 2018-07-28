898ab08a4a0d4613ea8dfdc1b48ea2ab5fa0b89d
J <jeames.busyqa@gmail.com>
2018-07-28 12:50:32 -0400

    change something

--- src/main/java/com/busyqa/coursework/wk1/CheckOddEven.java
+++ src/main/java/com/busyqa/coursework/wk1/CheckOddEven.java
@@ -2,7 +2,7 @@ package com.busyqa.coursework.wk1;
 
 public class CheckOddEven {
     public static void main(String[] args) {  // Program entry point
-        int number = 49;       // Set the value of "number" here!
+        int number = 50;       // Set the value of "number" here!
 //        System.out.println("The number is " + number);
 //        if ( ...... ) {
 //            System.out.println( ...... );
